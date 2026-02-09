e9b6031603982ee3c50aacefb1241cd2b812d25b
Marc Agen <marc.agen@epitech.eu>
2026-02-09 10:24:08 +0100

    [ADD]: adding task01

---
+++ task01.py
@@ -0,0 +1,12 @@
+##
+## EPITECH PROJECT, 2026
+## day01
+## File description:
+## task01
+##
+
+def print_transactions(lst: list[int | float]) -> None:
+    for i in list:
+        if i == 0:
+            continue
+        print(f"You spent {i} euros\n" if (i < 0) else f"You received {i} euros")
