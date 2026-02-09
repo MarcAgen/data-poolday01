bc7f018a109ea9c82b9e9d59098dceb0b7bde89e
Marc Agen <marc.agen@epitech.eu>
2026-02-09 10:24:51 +0100

    [FIX]: fixing the newline if value > 0

--- task01.py
+++ task01.py
@@ -9,4 +9,4 @@ def print_transactions(lst: list[int | float]) -> None:
     for i in list:
         if i == 0:
             continue
-        print(f"You spent {i} euros\n" if (i < 0) else f"You received {i} euros")
+        print(f"You spent {i} euros\n" if (i < 0) else f"You received {i} euros\n")
